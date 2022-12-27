7cf1337f5b4c06a6dba803768ff954d5f194b85f
Mark Foster <markdouglasfoster@gmail.com>
2022-12-27 17:18:42 -0600

    Update visualization function

--- explore.py
+++ explore.py
@@ -44,28 +44,36 @@ def compare_daily_stats(df):
     ax[0].plot(df.steps)
     ax[0].set_title('Total Steps')
     ax[0].set_ylabel('Steps')
+    ax[0].axhline(df.steps.mean(), color = 'r', linestyle = '--', label=f'{round(df.steps.mean(),2)} steps')
+    ax[0].legend(loc = 'upper right', frameon=True)
 
     ax[1].plot(df.weight)
     ax[1].set_title('Weight')
     ax[1].set_ylabel('kg')
+    ax[1].axhline(df.weight.mean(), color = 'r', linestyle = '--', label=f'{round(df.weight.mean(),2)} kg')
+    ax[1].legend(loc = 'upper right', frameon=True)
 
     ax[2].plot(df.resting_hr)
     ax[2].set_title('Resting HR')
     ax[2].set_ylabel('bpm')
+    ax[2].axhline(df.resting_hr.mean(), color = 'r', linestyle = '--', label=f'{round(df.resting_hr.mean(),2)} bpm')
+    ax[2].legend(loc = 'upper right', frameon=True)
 
     ax[3].plot(df.exercise_time)
     ax[3].set_title('Exercise Time')
     ax[3].set_ylabel('minutes')
+    ax[3].axhline(df.exercise_time.mean(), color = 'r', linestyle = '--', label=f'{round(df.exercise_time.mean(),2)} min')
+    ax[3].legend(loc = 'upper right', frameon=True)
 
     plt.show()
 
 def show_daily_viz(df, year=0, month=0):
     if (year > 2010) & (month > 0) & (month <=12):
-        df = df[f'{year}-{month}']
+        df = df[f"{year}-{month}"]
         compare_daily_stats(df)
 
     elif (year > 2010):
-        df = df[f'{year}']
+        df = df[f"{year}"]
         compare_daily_stats(df)
     else:
         compare_daily_stats(df)
